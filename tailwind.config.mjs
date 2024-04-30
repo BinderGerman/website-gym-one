aaf59c2d13bd7059bdbe55c291f8e7cc685f5ecf
BinderGerman <bindergerman@gmail.com>
2024-04-30 19:32:37 -0300

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F4AF00",
+        secondary: "#F4EBE0",
+        background: "#12100E",
+        accent: "#3D3D3D",
+      },
+    },
+  },
+  plugins: [],
+};
