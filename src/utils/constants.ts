6b56a5b0d66845545fd94563bebce76d9bce5d22
BinderGerman <bindergerman@gmail.com>
2024-05-09 22:38:14 -0300

    Created Section Characteristics

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,24 @@ export const Data = {
         title: "Quienes Somos",
         subtitle: 'Sobre Nosotros',
         description: '¡Bienvenido a [tu gimnasio]! Somos un equipo apasionado dedicado a tu bienestar y salud física. En nuestro espacio, encontrarás no solo equipos de última generación, sino también un ambiente cálido y acogedor donde cada miembro de nuestra comunidad se siente valorado y motivado. \n Nuestro objetivo es acompañarte en tu viaje hacia una vida más activa y saludable. ¡Únete a nosotros y descubre tu mejor versión!'
-    }
+    },
+
+    characteristics: [
+        {
+            title: 'Pro Equipment',
+            description: 'Equipamiento profesional para resultados óptimos.'
+        },
+        {
+            title: 'Ninja Coach',
+            description: 'Entrenadores expertos: tu camino hacia el éxito.'
+        },
+        {
+            title: 'Cardio First',
+            description: 'Priorizamos tu salud cardiovascular desde el principio.'
+        },
+        {
+            title: 'Flexible Time',
+            description: 'Horarios adaptables para tu vida activa.'
+        },
+    ]
 }
--- src/utils/functions/getData.ts
+++ src/utils/functions/getData.ts
@@ -3,3 +3,7 @@ import { Data } from '@/utils/constants'
 export const getAbout = () => {
     return Data.about
 }
+
+export const getCharacteristics = () => {
+    return Data.characteristics
+}
